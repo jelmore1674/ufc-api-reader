3fe7c52facbabab59d83639187dd197e5b3a4792
Justin Elmore <jelmore1674@pm.me>
2022-08-13 13:23:01 -0500

    refactor and add comments

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ import 'dotenv/config';
 import knex from 'knex';
 import ora from 'ora';
 
+// Connect to database
 const pg = knex({
 	client: 'pg',
 	connection: process.env.DB_URL,
 	searchPath: ['knex', 'public'],
 });
 
+// events stores only events that have not happened yet
 let events = [];
-let count = 1090;
-let apiError = false;
+// starting point to search events by id
+let eventId = 1090;
+// for the do while... keeps running as long as event is not empty
+let emptyEvent = false;
 
-const throbber = ora({
+// Conosle animation
+const scanning = ora({
 	color: 'red',
 	prefixText: 'Scanning database',
 	spinner: {
@@ -24,27 +29,26 @@ const throbber = ora({
 
 try {
 	do {
+		// fetch event from api
 		const response = await axios.get(
-			`https://d29dxerjsp82wz.cloudfront.net/api/v3/event/live/${count}.json`
+			`https://d29dxerjsp82wz.cloudfront.net/api/v3/event/live/${eventId}.json`
 		);
-		const { data } = response;
-		const { LiveEventDetail } = data;
-		if (LiveEventDetail.EventId) {
-			count = count + 1;
-			if (
-				LiveEventDetail.Name.match(/UFC /gi) &&
-				Date.parse(LiveEventDetail.StartTime) > new Date()
-			) {
+		const { EventId, Name, StartTime } = response.data.LiveEventDetail;
+		// Checking if there is a EventId
+		if (EventId) {
+			eventId = eventId + 1;
+			// Only will add events that have not taken place yet.
+			if (Name.match(/UFC /gi) && Date.parse(StartTime) > new Date()) {
 				events.push(data);
 			}
 		} else {
-			apiError = true;
-			throbber.succeed();
+			emptyEvent = true;
+			scanning.succeed();
 		}
-	} while (!apiError);
+	} while (!emptyEvent);
 } catch (err) {
+	scanning.fail();
 	console.error(err);
-	throbber.fail(err);
 }
 
 const sorting = ora({
@@ -55,7 +59,8 @@ const sorting = ora({
 		frames: ['.', '..', '...', ' '],
 	},
 }).start();
-events.sort(
+// sort events by event date
+events = events.sort(
 	(a, b) =>
 		new Date(a.LiveEventDetail.StartTime) -
 		new Date(b.LiveEventDetail.StartTime)
@@ -72,14 +77,16 @@ const database = ora({
 }).start();
 
 try {
+	// insert events into the database
 	await Promise.all(
 		events.map(async ({ LiveEventDetail }) => {
+			const { EventId, Name, Status, StartTime } = LiveEventDetail;
 			await pg
 				.insert({
-					eventid: LiveEventDetail.EventId,
-					name: LiveEventDetail.Name,
-					eventdate: LiveEventDetail.StartTime,
-					status: LiveEventDetail.Status,
+					eventid: EventId,
+					name: Name,
+					status: Status,
+					eventdate: StartTime,
 					added: pg.fn.now(),
 				})
 				.into('events')
