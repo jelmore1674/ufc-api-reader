ec60c9f60edc5e3f03d8b3d86814ac17bb197a2d
Justin Elmore <jelmore1674@pm.me>
2022-08-12 21:24:10 -0500

    add timestamp to verify it is updating

--- index.mjs
+++ index.mjs
@@ -9,94 +9,91 @@ const pg = knex({
 	searchPath: ['knex', 'public'],
 });
 
-async function fetchData() {
-	let events = [];
-	let count = 1090;
-	let apiError = false;
+let events = [];
+let count = 1090;
+let apiError = false;
 
-	const throbber = ora({
-		color: 'red',
-		prefixText: 'Scanning database',
-		spinner: {
-			interval: 180, // Optional
-			frames: ['.', '..', '...', ' '],
-		},
-	}).start();
+const throbber = ora({
+	color: 'red',
+	prefixText: 'Scanning database',
+	spinner: {
+		interval: 180, // Optional
+		frames: ['.', '..', '...', ' '],
+	},
+}).start();
 
-	try {
-		do {
-			const response = await axios.get(
-				`https://d29dxerjsp82wz.cloudfront.net/api/v3/event/live/${count}.json`
-			);
-			const { data } = response;
-			const { LiveEventDetail } = data;
-			if (LiveEventDetail.EventId) {
-				count = count + 1;
-				if (
-					LiveEventDetail.Name.match(/UFC /gi) &&
-					Date.parse(LiveEventDetail.StartTime) > new Date()
-				) {
-					events.push(data);
-				}
-			} else {
-				apiError = true;
-				throbber.succeed();
-			}
-		} while (!apiError);
-	} catch (err) {
-		console.error(err);
-		throbber.fail(err);
-	}
-
-	const sorting = ora({
-		color: 'red',
-		prefixText: 'Sorting events',
-		spinner: {
-			interval: 180, // Optional
-			frames: ['.', '..', '...', ' '],
-		},
-	}).start();
-	events.sort(
-		(a, b) =>
-			new Date(a.LiveEventDetail.StartTime) -
-			new Date(b.LiveEventDetail.StartTime)
-	);
-	sorting.succeed();
-
-	const database = ora({
-		color: 'red',
-		prefixText: 'Adding to database',
-		spinner: {
-			interval: 180, // Optional
-			frames: ['.', '..', '...', ' '],
-		},
-	}).start();
-
-	try {
-		await Promise.all(
-			events.map(async ({ LiveEventDetail }) => {
-				let event = await pg('events').where({
-					eventid: LiveEventDetail.EventId,
-					name: LiveEventDetail.Name,
-				});
-
-				if (!event.length) {
-					console.log(LiveEventDetail.Name);
-					await pg
-						.insert({
-							eventid: LiveEventDetail.EventId,
-							name: LiveEventDetail.Name,
-						})
-						.into('events');
-				}
-			})
+try {
+	do {
+		const response = await axios.get(
+			`https://d29dxerjsp82wz.cloudfront.net/api/v3/event/live/${count}.json`
 		);
-	} catch (e) {
-		database.fail();
-	} finally {
-		database.succeed();
-		process.exit(0);
-	}
+		const { data } = response;
+		const { LiveEventDetail } = data;
+		if (LiveEventDetail.EventId) {
+			count = count + 1;
+			if (
+				LiveEventDetail.Name.match(/UFC /gi) &&
+				Date.parse(LiveEventDetail.StartTime) > new Date()
+			) {
+				events.push(data);
+			}
+		} else {
+			apiError = true;
+			throbber.succeed();
+		}
+	} while (!apiError);
+} catch (err) {
+	console.error(err);
+	throbber.fail(err);
 }
 
-fetchData();
+const sorting = ora({
+	color: 'red',
+	prefixText: 'Sorting events',
+	spinner: {
+		interval: 180, // Optional
+		frames: ['.', '..', '...', ' '],
+	},
+}).start();
+events.sort(
+	(a, b) =>
+		new Date(a.LiveEventDetail.StartTime) -
+		new Date(b.LiveEventDetail.StartTime)
+);
+sorting.succeed();
+
+const database = ora({
+	color: 'red',
+	prefixText: 'Adding to database',
+	spinner: {
+		interval: 180, // Optional
+		frames: ['.', '..', '...', ' '],
+	},
+}).start();
+
+try {
+	await Promise.all(
+		events.map(async ({ LiveEventDetail }) => {
+			await pg('events').where({
+				eventid: LiveEventDetail.EventId,
+				name: LiveEventDetail.Name,
+			});
+
+			await pg
+				.insert({
+					eventid: LiveEventDetail.EventId,
+					name: LiveEventDetail.Name,
+					added: pg.fn.now(),
+				})
+				.into('events')
+				.onConflict('eventid')
+				.merge(['name', 'added']);
+		})
+	);
+	database.succeed();
+} catch (e) {
+	console.log(e);
+	database.fail();
+} finally {
+	process.exit(0);
+}
